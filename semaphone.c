0ce001877d829e72faa9b4f48af523de73aee127
VictoriaSzy <vszymanowski00@stuy.edu>
2019-12-23 17:11:52 -0500

    Started working on playing telephone

--- semaphone.c
+++ semaphone.c
@@ -1,5 +1,18 @@
 #include "control.h"
 
 int main() {
+  int semd = semget(KEY, 1, 0);
+  struct sembuf sb ;
+  sb.sem_num = 0 ;
+  sb.sem_op = -1 ;
+  sb.sem_flg = SEM_UNDO ;
+  int sem_opp = semop(semd, sb, 1);
+
+  int shmd = shmget(KEY, sizeof(int), 0644) ;
+  if (shmd == -1) {
+    printf("Error with shmget: %s\n", strerror(errno)) ;
+    return 1 ;
+  }
+
   return 0 ;
 }
