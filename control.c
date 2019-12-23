cc26dbd29f37fc763fc2459b6b2842b3ad23b653
VictoriaSzy <vszymanowski00@stuy.edu>
2019-12-23 16:06:09 -0500

    Finished up create function

--- control.c
+++ control.c
@@ -18,3 +18,24 @@ union semun {
   unsigned short *array ;
   struct seminfo *__buf ;
 } ;
+
+void create() {
+  printf("We are going to create the story now!\n") ;
+  int shmid = shmget(KEY, sizeof(int), 0644 | IPC_CREAT) ;
+  if (shmid == -1) printf("Error with creating shared memory! %d: %s\n", errno , strerror(errno)) ;
+
+  int fd = open("story.txt", O_CREAT | O_TRUNC, 0644) ;
+  if (fd == -1) printf("Error %d: %s\n", errno , strerror(errno)) ;
+
+  int semd = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644) ;
+  if (semd == -1) printf("Error %d: %s\n", errno , strerror(errno)) ;
+  
+  union semun us ;
+  us.val = 1 ;
+  int r = semctl(semd, 0, SETVAL, us) ;
+  if (r == -1) printf("Error %d: %s\n", errno, strerror(errno)) ;
+}
+
+int main() {
+  return 0 ;
+}
